d6542f845c361faebb463ed368585814353c6ffd
lfx <1274740448@qq.com>
2022-05-06 22:51:55 +0800

    测试

---
+++ 202104/src/Test.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("hello");
+        String s = "123";
+        s.isEmpty();
+        Date date = new Date();
+
+    }
+}
